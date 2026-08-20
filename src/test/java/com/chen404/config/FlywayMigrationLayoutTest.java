b6fcf7d2a2beea05353e446a313caccba9e9a444
yanhaochen <yanhaochen@ysbang.cn>
2026-08-20 14:43:34 +0800

    fix: 修复构建失败

--- src/test/java/com/chen404/config/FlywayMigrationLayoutTest.java
+++ src/test/java/com/chen404/config/FlywayMigrationLayoutTest.java
@@ -47,7 +47,8 @@ class FlywayMigrationLayoutTest {
             "V2026081001__admin_managed_feature_toggles.sql",
             "V2026081301__enable_admin_notification_by_default.sql",
             "V2026081701__remove_github_development_commit_limit.sql",
-            "V2026081702__create_reader_note.sql"
+            "V2026081702__create_reader_note.sql",
+            "V2026082001__add_reader_reading_mode.sql"
     );
 
     @Test
